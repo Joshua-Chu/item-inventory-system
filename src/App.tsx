d3884e45dbcf4bc5fdfbaf108530d055f5069192
Joshua-Chu <jpchu@up.edu.ph>
2022-03-10 18:39:41 +0800

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <h1 className="text-3xl bg-green-400 text-black text-center p-[48px]">
+            Hello World
+            <span>Joshua Chu handsome</span>
+        </h1>
+    );
+}
+
+export default App;
