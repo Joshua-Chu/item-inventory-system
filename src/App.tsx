2b5b26e4381f16e76b8834ae5e1cd6ee204ce80c
Joshua-Chu <jpchu@up.edu.ph>
2022-03-10 19:28:10 +0800

    add husky to project

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
         <h1 className="text-3xl bg-green-400 text-black text-center p-[48px]">
             Hello World
             <span>Joshua Chu handsome</span>
+            <span>another test</span>
         </h1>
     );
 }
